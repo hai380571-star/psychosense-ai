74618bd44dcff9dfb8aeb01c637068a5a9e82265
hai380571-star <hai380571@gmail.com>
2026-04-18 00:05:25 +0530

    Create script.js

---
+++ static/script.js
@@ -0,0 +1,68 @@
+body {
+    margin:0;
+    font-family:sans-serif;
+    background:#0f172a;
+    color:white;
+    display:flex;
+    flex-direction:column;
+    height:100vh;
+}
+
+/* HEADER */
+.header {
+    padding:12px;
+    background:#020617;
+    flex-shrink:0;
+}
+
+/* CHAT AREA */
+#chat {
+    flex:1;
+    overflow-y:auto;
+    padding:10px;
+    display:flex;
+    flex-direction:column;
+}
+
+/* MESSAGES */
+.msg {
+    padding:10px;
+    margin:6px;
+    border-radius:12px;
+    max-width:75%;
+    word-wrap:break-word;
+}
+
+.user {
+    background:#22c55e;
+    margin-left:auto;
+}
+
+.bot {
+    background:#1e293b;
+}
+
+/* FOOTER INPUT */
+.footer {
+    display:flex;
+    padding:10px;
+    background:#020617;
+    flex-shrink:0;
+}
+
+input {
+    flex:1;
+    padding:12px;
+    border-radius:20px;
+    border:none;
+    outline:none;
+}
+
+button {
+    margin-left:5px;
+    padding:10px 14px;
+    background:#22c55e;
+    color:white;
+    border:none;
+    border-radius:20px;
+    }
